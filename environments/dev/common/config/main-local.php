fb581b81b8860b05568b4b6334254289a74bf245
kz <784248377@qq.com>
2016-09-09 22:52:42 +0800

    unset config options which unnecessary in wsserver

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -16,5 +16,50 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
-    ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+        ],
+        'wsserver' => [
+            'class' => 'wsserver\server\WsServer'
+        ],
+        'event' => [
+            'class' => 'wsserver\base\Event',
+            'install' => [
+                ['common\models\Chat', 'installEventHandler'],
+                ['common\models\Chanel', 'installEventHandler'],
+            ],
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        'req' => [
+            'class' => 'wsserver\base\Req'
+        ],
+        'msg' => [
+            'class' => 'wsserver\base\Msg',
+        ],
+        'user' => [
+            'class' => 'wsserver\base\User',
+            'identityClass' => 'common\models\User'
+        ],
+        'auth' => [
+            'class' => 'wsserver\base\Auth',
+            'roleAssign' => [
+                'admin-role' => ['assignId' => 1, 'summary' => '管理员角色'],
+                'normal-role' => ['assignId' => 2, 'summary' => '一般用户角色'],
+            ],
+            'permissionAssign' => [
+                'admin-role' => "*",
+                'normal-role' => [
+                    'chanel' => [
+                        'join-public-chanel',
+                    ],
+                    'user' => "*",
+                ],
+            ],
+        ],
+    ]
 ];
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,14 +3,63 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=common-chat',
             'username' => 'root',
-            'password' => '',
+            'password' => '123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
         ],
-    ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+        ],
+        'wsserver' => [
+            'class' => 'wsserver\server\WsServer'
+        ],
+        'event' => [
+            'class' => 'wsserver\base\Event',
+            'install' => [
+                ['common\models\Chat', 'installEventHandler'],
+                ['common\models\Chanel', 'installEventHandler'],
+            ],
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        'req' => [
+            'class' => 'wsserver\base\Req'
+        ],
+        'msg' => [
+            'class' => 'wsserver\base\Msg',
+        ],
+        'user' => [
+            'class' => 'wsserver\base\User',
+            'identityClass' => 'common\models\User'
+        ],
+        'auth' => [
+            'class' => 'wsserver\base\Auth',
+            'roleAssign' => [
+                'admin-role' => ['assignId' => 1, 'summary' => '管理员角色'],
+                'normal-role' => ['assignId' => 2, 'summary' => '一般用户角色'],
+            ],
+            'permissionAssign' => [
+                'admin-role' => "*",
+                'normal-role' => [
+                    'chanel' => [
+                        'join-public-chanel',
+                    ],
+                    'user' => "*",
+                ],
+            ],
+        ],
+    ]
 ];
